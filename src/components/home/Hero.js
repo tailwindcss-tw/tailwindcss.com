3a599705e36cd73d7dce5f5976084b707132d3ba
huibizhang <naruto6404@hotmail.com>
2021-04-14 06:24:46 +0800

    Update Installation and Utility-first pages. Bugs fixing.
    Updated: Installation and utility-first pages.
    
    Bug Fixed: documentsPage link in Index.

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -364,9 +364,9 @@ export function Hero() {
                 >
                   <motion.div layout={layout} className="mb-4" transition={TRANSITION}>
                     <Words bolder={step >= 3} layout={layout} transition={TRANSITION}>
-					  “Tailwind CSS 是我見過唯一一個可以在大型團隊中
-            擴展開來的框架。它可以輕鬆的客製化、
-            適應任何設計，而且建置後又很輕巧。”
+					            “Tailwind CSS 是我見過唯一一個可以在大型團隊中
+                      擴展開來的框架。它可以輕鬆的客製化、
+                      適應任何設計，而且建置後又很輕巧。”
                     </Words>
                   </motion.div>
                   <motion.div
