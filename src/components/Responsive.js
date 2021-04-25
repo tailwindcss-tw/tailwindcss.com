9b43f0b21ff87e762020ce96c4a07a3fc38e1ff4
Augustine <ashramwen@gmail.com>
2021-04-25 11:10:35 +0800

    refactor: 💡 Extract responsive

---
+++ src/components/Responsive.js
@@ -0,0 +1,13 @@
+export function Responsive() {
+  // For more information about Tailwind's responsive design features, check out the [ResponsiveDesign](/docs/responsive-design) documentation.
+
+  return (
+    <div className="prose">
+      <p>
+        更多有關 Tailwind 響應式設計功能的資訊，可參考
+        {' '}<a href="/docs/responsive-design">響應式設計</a>{' '}
+        文件。
+      </p>
+    </div>
+  )
+}
