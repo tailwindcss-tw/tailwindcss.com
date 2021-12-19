889bda33d5cb9c8b54bbc78b4cf748a17d1b84b7
huibizhang <naruto6404@hotmail.com>
2021-12-20 04:30:11 +0800

    copy components to components/.ZH

---
+++ src/components/.ZH/Header.js
@@ -0,0 +1,275 @@
+import Link from 'next/link'
+import { VersionSwitcher } from '@/components/VersionSwitcher'
+// import { SearchButton } from '@/components/Search'
+import { SearchButton } from '@/components/.ZH/Search'
+import Router from 'next/router'
+import { Logo } from '@/components/Logo'
+import { Dialog } from '@headlessui/react'
+import { useEffect, useState } from 'react'
+import clsx from 'clsx'
+
+function Featured() {
+  return (
+    <Link href="/blog/tailwindcss-v3">
+      <a className="ml-3 text-xs leading-5 text-sky-600 bg-sky-400/10 rounded-full py-1 px-3 hidden xl:flex items-center hover:bg-sky-400/20">
+        <strong className="font-semibold">Tailwind CSS v3.0</strong>
+        <svg width="2" height="2" fill="currentColor" aria-hidden="true" className="ml-2">
+          <circle cx="1" cy="1" r="1" />
+        </svg>
+        <span className="ml-2">
+          {/* Just-in-Time all the time, colored shadows, scroll snap and more */}
+          Just-in-Time all the time, colored shadows, scroll snap and more
+        </span>
+        <svg width="3" height="6" className="ml-3 overflow-visible text-sky-300" aria-hidden="true">
+          <path
+            d="M0 0L3 3L0 6"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </a>
+    </Link>
+  )
+}
+
+export function NavPopover() {
+  let [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    if (!isOpen) return
+    function handleRouteChange() {
+      setIsOpen(false)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [isOpen])
+
+  return (
+    <>
+      <button
+        type="button"
+        className="text-gray-500 w-8 h-8 flex items-center justify-center hover:text-gray-600"
+        onClick={() => setIsOpen(true)}
+      >
+        {/* <span className="sr-only">Navigation</span> */}
+        <span className="sr-only">導航欄</span>
+        <svg width="24" height="24" fill="none" aria-hidden="true">
+          <path
+            d="M12 6v.01M12 12v.01M12 18v.01M12 7a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </button>
+      <Dialog as="div" className="fixed z-50 inset-0 md:hidden" open={isOpen} onClose={setIsOpen}>
+        <Dialog.Overlay className="fixed inset-0 bg-black/20 backdrop-blur-sm" />
+        <div className="fixed top-4 right-4 w-full max-w-xs bg-white rounded-lg shadow-lg p-6 text-base font-semibold text-gray-900">
+          <button
+            type="button"
+            className="absolute top-5 right-5 w-8 h-8 flex items-center justify-center text-gray-500 hover:text-gray-600"
+            onClick={() => setIsOpen(false)}
+          >
+            {/* <span className="sr-only">Close navigation</span> */}
+            <span className="sr-only">關閉導航欄</span>
+            <svg viewBox="0 0 10 10" className="w-2.5 h-2.5 overflow-visible" aria-hidden="true">
+              <path
+                d="M0 0L10 10M10 0L0 10"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+            </svg>
+          </button>
+          <ul className="space-y-6">
+            <NavItems />
+          </ul>
+        </div>
+      </Dialog>
+    </>
+  )
+}
+
+export function NavItems() {
+  return (
+    <>
+      <li>
+        <Link href="/docs/.ZH/installation">
+          {/* <a className="hover:text-sky-500">Docs</a> */}
+          <a className="hover:text-sky-500">文件</a>
+        </Link>
+      </li>
+      <li>
+        <a href="https://tailwindui.com" className="hover:text-sky-500">
+          {/* Components */}
+          元件庫
+        </a>
+      </li>
+      <li>
+        <Link href="/blog">
+          {/* <a className="hover:text-sky-500">Blog</a> */}
+          <a className="hover:text-sky-500">部落格</a>
+        </Link>
+      </li>
+      <li>
+        <a
+          href="https://github.com/tailwindlabs/tailwindcss"
+          className="block w-6 h-6 text-gray-400 hover:text-gray-500"
+        >
+          <span className="sr-only">Tailwind CSS on GitHub</span>
+          <svg viewBox="0 0 16 16" width="24" height="24" fill="currentColor" aria-hidden="true">
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
+      </li>
+    </>
+  )
+}
+
+export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section }) {
+  let [isOpaque, setIsOpaque] = useState(false)
+
+  useEffect(() => {
+    let offset = 50
+    function onScroll() {
+      if (!isOpaque && window.scrollY > offset) {
+        setIsOpaque(true)
+      } else if (isOpaque && window.scrollY <= offset) {
+        setIsOpaque(false)
+      }
+    }
+    window.addEventListener('scroll', onScroll, { passive: true })
+    return () => {
+      window.removeEventListener('scroll', onScroll, { passive: true })
+    }
+  }, [isOpaque])
+
+  return (
+    <>
+      <div className="absolute z-20 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
+        <div className="w-[108rem] flex-none flex justify-end">
+          <picture>
+            <source srcSet={require('@/img/beams/docs@30.avif').default} type="image/avif" />
+            <img
+              src={require('@/img/beams/docs@tinypng.png').default}
+              alt=""
+              className="w-[71.75rem] flex-none max-w-none"
+            />
+          </picture>
+        </div>
+      </div>
+      <div
+        className={clsx(
+          'sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10',
+          isOpaque
+            ? 'bg-white supports-backdrop-blur:bg-white/95'
+            : 'bg-white/95 supports-backdrop-blur:bg-white/60'
+        )}
+      >
+        <div className="max-w-8xl mx-auto">
+          <div
+            className={clsx(
+              'py-4 border-b border-gray-900/10 lg:px-8 lg:border-0',
+              hasNav ? 'mx-4 lg:mx-0' : 'px-4'
+            )}
+          >
+            <div className="relative flex items-center">
+              <Link href="/">
+                <a
+                  className="mr-3 flex-none w-[2.0625rem] overflow-hidden md:w-auto"
+                  onContextMenu={(e) => {
+                    e.preventDefault()
+                    Router.push('/brand')
+                  }}
+                >
+                  {/* <span className="sr-only">Tailwind CSS home page</span> */}
+                  <span className="sr-only">Tailwind CSS 首頁</span>
+                  <Logo className="w-auto h-5" />
+                </a>
+              </Link>
+              <VersionSwitcher />
+              <Featured />
+              <nav className="hidden lg:block ml-auto">
+                <ul className="flex space-x-8 text-sm leading-6 font-semibold text-gray-700">
+                  <NavItems />
+                </ul>
+              </nav>
+              <SearchButton className="ml-auto text-gray-500 w-8 h-8 -my-1 flex items-center justify-center hover:text-gray-600 lg:hidden">
+                {/* <span className="sr-only">Search</span> */}
+                <span className="sr-only">搜尋</span>
+                <svg
+                  width="24"
+                  height="24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  aria-hidden="true"
+                >
+                  <path d="m19 19-3.5-3.5" />
+                  <circle cx="11" cy="11" r="6" />
+                </svg>
+              </SearchButton>
+              <div className="ml-2 -my-1 lg:hidden">
+                <NavPopover />
+              </div>
+            </div>
+          </div>
+          {hasNav && (
+            <div className="flex items-center p-4 border-b border-gray-900/10 lg:hidden">
+              <button
+                type="button"
+                onClick={() => onNavToggle(!navIsOpen)}
+                className="text-gray-500 hover:text-gray-600"
+              >
+                {/* <span className="sr-only">Navigation</span> */}
+                <span className="sr-only">導航欄</span>
+                <svg width="24" height="24">
+                  <path
+                    d="M5 6h14M5 12h14M5 18h14"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </button>
+              {title && (
+                <ol className="ml-4 flex text-sm leading-6 whitespace-nowrap min-w-0">
+                  {section && (
+                    <li className="flex items-center">
+                      {section}
+                      <svg
+                        width="3"
+                        height="6"
+                        aria-hidden="true"
+                        className="mx-3 overflow-visible text-gray-400"
+                      >
+                        <path
+                          d="M0 0L3 3L0 6"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                        />
+                      </svg>
+                    </li>
+                  )}
+                  <li className="font-semibold text-gray-900 truncate">{title}</li>
+                </ol>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
