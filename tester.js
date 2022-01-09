3c05b75055b1a35dfc230d763748d10d8e4b8e73
搋兔 <naruto6404@hotmail.com>
2022-01-09 16:08:17 +0800

    add base-dir support and pathType checking

--- tester.js
+++ tester.js
@@ -2,15 +2,17 @@ const { getOptions } = require("loader-utils")
 var fs = require('fs')
 
 function file_replacer (source) {
+  const base = getOptions(this).base
   const list = getOptions(this).replacement
 
   const filename = this.resourcePath.split(/[\/\\]+/)
-  const save_filename = filename[filename.length-1]
 
-  
-  if(fs.existsSync(list[save_filename])){
-    console.log(`[replacer] replaced '${save_filename}' to '${list[save_filename]}'`)
-    return fs.readFileSync(list[save_filename])
+  const save_filename = filename[filename.length-1]
+  const target_filename = base ? [base,list[save_filename]].join("/") : list[save_filename]
+
+  if(fs.existsSync(target_filename) && fs.lstatSync(target_filename).isFile()){
+    console.log(`[replacer] replaced '${save_filename}' to '${target_filename}'`)
+    return fs.readFileSync(target_filename)
   } else {
     // console.log(`[replacer] replace failed.`)
     return source
