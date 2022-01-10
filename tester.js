3706c1ff7bf7988c64bedbceb824f6f27ab12943
搋兔 <naruto6404@hotmail.com>
2022-01-11 02:56:23 +0800

    fix coding style

--- tester.js
+++ tester.js
@@ -4,28 +4,25 @@ const fsPromises = require('fs').promises
 const path = require('path')
 
 async function fileReplacer (source) {
-  const base = getOptions(this).base
-  const list = getOptions(this).replacement
+  const { base, replacements } = getOptions(this)
   
   const filename = path.parse(this.resourcePath)
   
   const saveFilename = filename.base
-  const targetFilename = base ? path.join(base,String(list[saveFilename])) : list[saveFilename]
+  const targetFilename = base ? path.join(base,String(replacements[saveFilename])) : replacements[saveFilename]
 
   try{
     await fsPromises.access(targetFilename, fs.constants.R_OK | fs.constants.W_OK)
     const stat = await fsPromises.lstat(targetFilename)
-    
-    let translatedFileContent = ""
 
     if(stat.isFile()){
-      translatedFileContent = await fsPromises.readFile(targetFilename)
+      const translatedFileContent = await fsPromises.readFile(targetFilename)
+
+      console.log(`[file-replacer] replaced '${saveFilename}' to '${targetFilename}'`)
+      return translatedFileContent
     } else {
       throw `Target path "${targetFilename}" is not a file.`
     }
-    
-    console.log(`[file-replacer] replaced '${saveFilename}' to '${targetFilename}'`)
-    return translatedFileContent
   } catch(e) {
     // console.log(`[file-replacer] ${e}`)
     return source
