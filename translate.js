953b7038157c3245f51d27730a0a8464c408886f
Augustine <ashramwen@users.noreply.github.com>
2022-01-17 09:38:03 +0800

    feat: 🎸 try to translate file during compile time (#149)
    Co-authored-by: kayac-chang <egg734631@gmail.com>

--- translate.js
+++ translate.js
@@ -8,34 +8,5 @@ module.exports = () => {
         replace: "元件",
       },
     },
-    {
-      test: /\.mdx$/,
-      loader: "./file-replacer",
-      options: {
-        base: "./translated",
-        replacement: {
-          "editor-setup.mdx": "editor-setup.mdx",
-        },
-      },
-    },
-    {
-      test: /documentation\.js$/,
-      loader: "./file-replacer",
-      options: {
-        replacement: {
-          "documentation.js": "./translated/navs/documentation.js",
-        },
-      },
-    },
-    {
-      test: /components[\/+\\+]home[\/+\\+]\w*\.js$/,
-      loader: "./file-replacer",
-      options: {
-        base: "./translated/components/home",
-        replacement: {
-          "Hero.js": "Hero.js",
-        },
-      },
-    },
   ];
 };
